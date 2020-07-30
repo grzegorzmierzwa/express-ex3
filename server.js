ec5654f0f9a3e06bb23aa7cb9664775fde3a2801
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-07-30 06:56:21 +0200

    Expoert database to external file

--- server.js
+++ server.js
@@ -1,17 +1,56 @@
 const express = require('express');
 const app = express();
+const db= require('./db');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // app.use(cors());
 
 
-const db = [
-    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-    { id: 3, author: 'Greg Doe', text: 'This company is worth every coin!' },
-    { id: 4, author: 'Tom Doe', text: 'They really know how to make you happy.' },
-  ];
+app.get('/testimonials', (req, res) => {
+    res.json(db.testimonials);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    res.json(db.testimonials.find(item => item.id == req.params.id));
+    
+});
+
+app.get('/testimonials/random', (req, res) => {
+    res.json(db.testimonials[Math.floor(Math.random() * db.testimonials.length)]);
+});
+
+app.post('/testimonials', (req, res) => {
+    const {author, text} = req.body;
+    console.log(req.body);
+    const id = db.testimonials.length + 1;
+
+    db.testimonials.push({id, author, text});
+
+    res.json({ message: 'OK' });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+    const testimonial = db.testimonials.find(item => item.id == req.params.id);
+
+    if (!testimonial) {
+        res.status(404).json({message: 'Not found' });
+        return;
+    }
+
+    testimonial.author = req.body.author;
+    testimonial.text = req.body.text;
+    
+    res.json({ message: 'OK' });
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+    db = db.testimonials.filter((item) => item.id != req.params.id);
+
+    res.json({ message: 'OK' });
+});
+
+
 
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
